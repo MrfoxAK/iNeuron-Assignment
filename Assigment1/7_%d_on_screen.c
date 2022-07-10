d6a36c182dda0ec0381dd30c3ce38e2457b81459
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-07-10 21:22:54 +0530

    Add files via upload

---
+++ Assigment1/7_%d_on_screen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+     printf("%%d");
+     return 0;
+}
+// #include <stdio.h>
+// int main()
+// {
+//      char c[] = "%d";
+//      printf("%s",c);
+//      return 0;
+// }
