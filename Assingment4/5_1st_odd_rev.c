c3862a4b65216d5af2dfd27023a0d28bebf17a95
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-07-24 12:07:43 +0530

    Add files via upload

---
+++ Assingment4/5_1st_odd_rev.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+     for (int i = 20; i > 0 ; i--)
+     {
+          if (i%2==1)
+          {
+               printf("%d ",i);
+          }
+     }
+     return 0;
+}
