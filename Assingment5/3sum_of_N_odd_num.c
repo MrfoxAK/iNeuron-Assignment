83a5c5650017737be262e344152d93cec8510628
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-07-24 14:45:32 +0530

    Add files via upload

---
+++ Assingment5/3sum_of_N_odd_num.c
@@ -0,0 +1,17 @@
+// 3. Write a program to calculate sum of first N odd natural numbers
+#include <stdio.h>
+int main()
+{
+     int n, sum = 0;
+     printf("Enter N: ");
+     scanf("%d", &n);
+     for (int i = 1; i <= 2 * n; i++)
+     {
+          if (i % 2 == 1)
+          {
+               sum += i;
+          }
+     }
+     printf("The sum is: %d", sum);
+     return 0;
+}
