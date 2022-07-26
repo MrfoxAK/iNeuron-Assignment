8f27565d5a86ac7b8ea38604694cd917fe6c3204
MrfoxAK <95500036+MrfoxAK@users.noreply.github.com>
2022-07-26 21:10:15 +0530

    Add files via upload

---
+++ Assingment8/7_star_pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+     int n;
+     printf("Enter the no.of rows : ");
+     scanf("%d",&n);
+     for (int i = 0; i <= n; i++)
+     {
+          for (int j = 0; j <= 2*n; j++)
+          {
+               if (j<=n-i || j>=n+i)
+               {
+                    printf("*");
+               }
+               else
+               {
+                    printf(" ");
+               }
+          }
+          printf("\n");
+     }
+     return 0;
+}
